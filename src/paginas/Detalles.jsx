444a3a4934555d48325fb959c972011b73d191a6
alx2410 <alegonzalesvaez@crackthecode.la>
2025-10-03 11:34:06 -0500

    arreglo

--- src/paginas/Detalles.jsx
+++ src/paginas/Detalles.jsx
@@ -1,25 +1,22 @@
 import { useParams } from "react-router-dom";
 import { listaProductos} from "../assets/productos"
 
-export function Detalle(){
-    const {id} = useParams()
-    const producto = listaProductos.find(p=> p.id === Number(id))
+export function Detalles() {
+  const { id } = useParams();
+  const producto = listaProductos.find(p => p.id === Number(id));
 
-    //COMPROBACION SI EXISTE EL PRODUCTO
- if(!producto){
-        return(<div>Producto no existe</div>)
-    }
-    
-    return(
-        <div>
-            {
-                <>
-                <h1 id="detalle-title">{producto.nombre}</h1>
-                <p>{producto.descripcion}</p>
-                <p>{producto.precio}</p>
-                </>
-            }
-        </div>
-    )
+  if (!producto) {
+    return <div>Producto no existe</div>;
+  }
+
+  return (
+    <div>
+      <>
+        <h1 id="detalle-title">{producto.nombre}</h1>
+        <p>{producto.descripcion}</p>
+        <p>{producto.precio}</p>
+      </>
+    </div>
+  );
 
 }
